b896007e562bed5abf661e2546f74903ffe7ff15
ZhangZhao66 <49257978+ZhangZhao66@users.noreply.github.com>
2019-04-04 10:27:51 +0800

    Update Demo.java

--- GitHubTest/src/com/xdl/test/Demo.java
+++ GitHubTest/src/com/xdl/test/Demo.java
@@ -3,6 +3,9 @@ package com.xdl.test;
 public class Demo {
 
     public static void main(String[] args) {
+        /**
+         * HAHA
+         */
         System.out.println("Hello World!");
     }
 
